8ed52619e808c9c43a1813419826e10c75800042
ipritish <pritishjain2009@gmail.com>
2016-01-08 15:59:40 +0530

    maven project

--- src/com/datashow/ui/AppStarter.java
+++ src/com/datashow/ui/AppStarter.java
@@ -1,5 +1,23 @@
 package com.datashow.ui;
 
+import org.apache.log4j.Logger;
+
 public class AppStarter {
+	
+	private static final Logger logger = Logger.getLogger(AppStarter.class);
+
+	public static void main(String[] args) {
+		System.out.println(getLocalCurrentDate());
+	}
+
+	private static String getLocalCurrentDate() {
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("getLocalCurrentDate() is executed!");
+		}
+
+		return "today's date";
+
+	}
 
 }
