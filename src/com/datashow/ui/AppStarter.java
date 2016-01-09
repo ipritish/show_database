dd00b74c89954d30e615f4c02948cc807a61b9bf
ipritish <pritishjain2009@gmail.com>
2016-01-09 17:18:22 +0530

    first gui model

--- src/com/datashow/ui/AppStarter.java
+++ src/com/datashow/ui/AppStarter.java
@@ -1,6 +1,7 @@
 package com.datashow.ui;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import org.apache.log4j.Logger;
 
@@ -9,9 +10,20 @@ public class AppStarter {
 	private static final Logger logger = Logger.getLogger(AppStarter.class);
 	
 	JFrame mainFrame = new JFrame("Shows and Anime");
+	
+	public void showGui()
+	{
+		JPanel panel = new JPanel();
+		mainFrame.add(panel);
+		mainFrame.setVisible(true);
+		mainFrame.setLocation(20, 30);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 
 	public static void main(String[] args) {
-		System.out.println(getLocalCurrentDate());
+		//System.out.println(getLocalCurrentDate());
+		AppStarter start = new AppStarter();
+		start.showGui();
 	}
 
 	private static String getLocalCurrentDate() {
