ad7931d545e01ef9dd2d659cd357a31563bc0e77
ipritish <pritishjain2009@gmail.com>
2016-01-27 15:56:15 +0530

    gui project

--- src/main/java/com/datashow/constants/TableHeaders.java
+++ src/main/java/com/datashow/constants/TableHeaders.java
@@ -6,11 +6,8 @@ import java.util.Vector;
 public class TableHeaders {
 	
 
-	public static Vector<String> showColumns = new Vector<String>();
-	public static Vector<String> animeColumns = new Vector<String>();
-	
-	public static Vector<Vector<Object>> placeHolderAnimeRowsData = new Vector<Vector<Object>>();
-	public static Vector<Vector<Object>> placeHolderShowRowsData = new Vector<Vector<Object>>();
+	private static Vector<String> showColumns = new Vector<String>();
+	private static Vector<String> animeColumns = new Vector<String>();
 	
 	public static Vector<String> getShowColumns()
 	{
@@ -32,43 +29,6 @@ public class TableHeaders {
 		animeColumns.add("Is Airing");
 		animeColumns.add("Air Day");
 		return animeColumns;
-	}
-	
-	public static Vector<Vector<Object>> getPlaceHolderShowData()
-	{
-		placeHolderAnimeRowsData.removeAllElements();
-		Vector<Object> firstRowData = new Vector<Object>();
-		firstRowData.addElement("Unbrekable Kimmi Schimdt");
-		firstRowData.addElement(new Double(8.0));
-		firstRowData.addElement(new Integer(2));
-		firstRowData.addElement(new Boolean(false));
-		firstRowData.addElement("Sunday");
-		
-		Vector<Object> secondRowData = new Vector<Object>();
-		secondRowData.addElement("Archer");
-		secondRowData.addElement(new Double(8.9));
-		secondRowData.addElement(new Integer(7));
-		secondRowData.addElement(new Boolean(false));
-		secondRowData.addElement("Sunday");
-		
-		placeHolderAnimeRowsData.addElement(firstRowData);
-		placeHolderAnimeRowsData.addElement(secondRowData);
-		
-		return placeHolderAnimeRowsData;
-	}
-	
-	public static Vector<Vector<Object>> getPlaceHolderAnimeData()
-	{
-		placeHolderAnimeRowsData.removeAllElements();
-		Vector<Object> firstRowData = new Vector<Object>();
-		firstRowData.addElement("Naruto");
-		firstRowData.addElement(new Double(8.0));
-		firstRowData.addElement(new Integer(2));
-		firstRowData.addElement(new Boolean(true));
-		firstRowData.addElement("Thursday");
-		
-		placeHolderAnimeRowsData.addElement(firstRowData);
-		return placeHolderAnimeRowsData;
 	}
 
 }
